0efedfa8d07c180d05ac99273da5e4f1410cb74e
Rich Wandell <richwandell@gmail.com>
2022-02-13 20:02:05 -0500

    idk what im doing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,22 @@ const IndexPage = () => {
   return (
     <main >
       <title>Projects by Rich</title>
-        <Grid >
-            <Row >
-                <Col xs={2}>
-                    <Row >
-                        <Col xs={12}>
-                            <img id="profile-image" src={rich} />
-                        </Col>
-                    </Row>
-                    <Row></Row>
+        <Grid fluid={true} id={"header-container"}>
+            <Row id={"header-row"}>
+                <Col xs={9}>
+                    <img id="profile-image" src={rich} />
                 </Col>
-                <Col xs={10}>
-                    <Row>
-                        <Col xs={12}>
-                        <Grid fluid={true} >
-                            <Tabs id="TabsExample"  selectedTabId="rx">
-                                <Tab id="ng" title="Angular" panel={<div />} />
-                                <Tab id="mb" title="Ember" panel={<div />} panelClassName="ember-panel" />
-                                <Tab id="rx" title="React" panel={<div />} />
-                                <Tab id="bb" disabled title="Backbone" panel={<div />} />
-                                <Tabs.Expander />
-                                <input className="bp3-input" type="text" placeholder="Search..." />
-                            </Tabs>
-                        </Grid>
-                        </Col>
-                    </Row>
-                    <Row></Row>
+                <Col xs={3}>
+                    <Grid fluid={true} >
+                        <Tabs id="TabsExample"  selectedTabId="rx">
+                            <Tab id="ng" title="Angular" panel={<div />} />
+                            <Tab id="mb" title="Ember" panel={<div />} panelClassName="ember-panel" />
+                            <Tab id="rx" title="React" panel={<div />} />
+                            <Tab id="bb" disabled title="Backbone" panel={<div />} />
+                            <Tabs.Expander />
+                            <input className="bp3-input" type="text" placeholder="Search..." />
+                        </Tabs>
+                    </Grid>
                 </Col>
             </Row>
         </Grid>
